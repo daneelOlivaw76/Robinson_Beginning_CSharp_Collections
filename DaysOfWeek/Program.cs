019d7879483684323000c7419a953efdbb69a0ab
daneelOlivaw76 <ruimgreis@gmail.com>
2020-05-04 11:41:22 +0100

    Initial Commit - Middle of module 2

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pluralsight.BegCShCollections.DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wensday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            };
+
+            Console.WriteLine("Before:");
+
+            foreach (string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+
+            daysOfWeek[2] = "Wednesday";
+
+            Console.WriteLine("\r\nAfter:");
+
+            foreach ( string day in daysOfWeek)
+            {
+                Console.WriteLine(day);
+            }
+            
+        }
+    }
+}
---
+++ NthDay/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NthDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            };
+
+            Console.WriteLine($"Which day do you eant o display?");
+            Console.Write($"(Monday = 1, etc) > ");
+
+            int iDay = int.Parse(Console.ReadLine());
+
+            string chosenDay = daysOfWeek[iDay -1];
+            Console.WriteLine($"That day is {chosenDay}");
+        }
+    }
+}
