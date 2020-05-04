8de9624477e369de6e5aac7a6b0dca822cbc654f
daneelOlivaw76 <ruimgreis@gmail.com>
2020-05-04 17:37:20 +0100

    End of Module 4

---
+++ TopTenPops/CsvReader.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace Pluralsight.BegCShCollections.IntroColls.TopTenPops
+{
+    internal class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+
+            using (var reader = new StreamReader(this._csvFilePath))
+            {
+                // read header line
+                reader.ReadLine();
+
+                for(int i = 0; i < nCountries; i++)
+                {
+                    string line = reader.ReadLine();
+                    countries[i] = ReadCountryFromCsvLine(line);
+
+                }
+            }
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
---
+++ TopTenPops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace Pluralsight.BegCShCollections.IntroColls.TopTenPops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Rui Reis\program\learning\dotnet\pluralsight\Robinson_Beginning_CSharp_Collections\code\Pop by Largest Final.csv";
+
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: { country.Name}");
+            }
+        }
+    }
+}
