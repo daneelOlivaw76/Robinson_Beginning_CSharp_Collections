52b908460405a1d00ef0d7f9fe663578a216cbb9
daneelOlivaw76 <ruimgreis@gmail.com>
2020-05-05 12:21:11 +0100

    Middle of Module 5

---
+++ DictSnippets/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using Pluralsight.BegCShCollections.IntroColls.TopTenPops;
+
+namespace Pluralsight.BegCShCollections.DictSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country norway = new Country("Norway", "NOR", "EUROPE", 5_282_223);
+            Country finland = new Country("Finland", "FIN", "EUROPE", 5_511_303);
+
+            var countries = new Dictionary<string, Country>
+            {
+                {norway.Code, norway},
+                {finland.Code, finland}
+            };
+
+            Country selectedCountry = countries["NOR"];
+            WriteLine(selectedCountry.Name);
+
+            foreach(var country in countries.Values)
+            {
+                WriteLine(country.Name);
+            }
+
+            bool exists = countries.TryGetValue("MUS", out Country c);
+
+            if(exists)
+            {
+                WriteLine(c.Name);
+            }
+            else
+            {
+
+                WriteLine("There is no counry with the code MUS");
+            }
+        }
+    }
+}
